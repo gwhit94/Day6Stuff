d79770812e97098f8432d48d10c521516f063c2f
Graham W <greysnaa@gmail.com>
2019-07-22 09:37:59 -0500

    js object

--- IntroJS/script.js
+++ IntroJS/script.js
@@ -1,3 +1,11 @@
-var me = "Graham";
 
-console.log(me);
+var me = {
+    name: "Graham",
+    age: 25,
+    hair: true,
+    height: 62 + " in",
+    food: ["burgers", "hot dogs", "spaghetti"],
+};
+
+console.log(me.food[0]);
+console.log(me.height);
