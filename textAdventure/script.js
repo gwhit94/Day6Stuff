3c2a1b2f6a8dccbb202ebc562dfd982c2c6239e6
Graham W <greysnaa@gmail.com>
2019-07-22 23:59:53 -0500

    Rough Draft of textAdventure, but deadline met

--- textAdventure/script.js
+++ textAdventure/script.js
@@ -92,59 +92,67 @@ function choice5(){
     }
 }
 function storyGen(){
-    if (saveFile.classChoice == "THIEF"){
+    if (saveFile.class == "THIEF"){
         var thiefStory = `${saveFile.name} the ${saveFile.class}`;
+        console.log('test');
         if (saveFile.choiceLog[0] == 0){
-            thiefStory += `saw an OLD WOMAN begging with a HAT FULL OF CHANGE, and promptly decided to PILFER her of her LOOT. A THIEF indeed!`;
+            thiefStory += ` saw an OLD WOMAN begging with a HAT FULL OF CHANGE, and promptly decided to PILFER her of her LOOT. A THIEF indeed! The LOCAL TOWNSFOLK took pity on the OLD WOMAN after witnessing what you had done.`;
         } else if( saveFile.choiceLog[0] == 1){
-            thiefStory += `saw an OLD WOMAN begging with a HAT FULL OF CHANGE, they decided to DONATE their LAST COIN. How UNCHARACTERISTIC of a THIEF.`;
+            thiefStory += ` saw an OLD WOMAN begging with a HAT FULL OF CHANGE, they decided to DONATE their LAST COIN before moving on. How UNCHARACTERISTIC of a THIEF. The OLD WOMAN turned out to be a MASTER OF DISGUISE. Are you a THIEF or a FOOL?`;
+        };
+        // NOTE:
+        // It was at this point I realized if I wanted a unique story for each choice, I would need to write a large number of stories. If I had the time to do so I would, but this is an overnight project . .
+        // Henceforth, I will simply provide the standard actions chosen by the player. Less exciting, but deadline met! 
+        if (saveFile.choiceLog[1] == 0){
+            thiefStory += ` ${saveFile.name} the THIEF then upped their ANTE by STEALING a HORSE that had roamed away from its OWNER. A THIEF to the END! `;
+        } else if( saveFile.choiceLog[1] == 1){
+            thiefStory += ` Spotting a HORSE that had roamed away from its OWNER, ${saveFile.name} the THIEF decided to RETURN the HORSE to its HOME. `;
+        };
+        if (saveFile.choiceLog[2] == 0){
+            thiefStory += ` ${saveFile.name} the ${saveFile.class} found themselves in front of a RAGING RIVER. You FORDED the RIVER but LOST all of your POSSESSIONS! `;
+        } else if( saveFile.choiceLog[2] == 1){
+            thiefStory += ` ${saveFile.name} the ${saveFile.class} found themselves in front of a RAGING RIVER. You TURNED AWAY and STAYED DRY! `;
+        };
+        if (saveFile.choiceLog[3] == 0){
+            thiefStory += `${saveFile.name} the ${saveFile.class} heard a loud GROWL coming from the DARK WOODS. You tried to FIGHT the BEAST but you were too SCRAWNY being a THIEF and were WOUNDED. `;
+        } else if( saveFile.choiceLog[3] == 1){
+            thiefStory += `${saveFile.name} the ${saveFile.class} heard a loud GROWL coming from the DARK WOODS. You quickly ABSCOND, escaping any HARM. `;
+        };
+        if (saveFile.choiceLog[4] == 0){
+            thiefStory += `${saveFile.name} the ${saveFile.class} arrived at the ALTAR of a SACRED TEMPLE smelling faintly of INCENSE. You PRAY at the ALTAR but THE GODS do not answer the prayers of THIEVES.`;
+        } else if( saveFile.choiceLog[4] == 1){
+            thiefStory += `${saveFile.name} the ${saveFile.class} arrived at the ALTAR of a SACRED TEMPLE smelling faintly of INCENSE. You REST at the ALTAR, regaining your STRENGTH to BE A NO GOOD CROOK BUT STILL AN ALL AROUND COOL CHARACTER!`;
+        };
+        alert(thiefStory);
+    } else if (saveFile.class == "KNIGHT"){
+        var knightStory = `The tale of ${saveFile.name} the ${saveFile.class}. `;
+        if (saveFile.choiceLog[0] == 0){
+            knightStory += `${saveFile.name} the ${saveFile.class} saw an OLD WOMAN in the village square begging with a HAT FULL OF CHANGE. You ROBBED her of her COIN! `;
+        } else if( saveFile.choiceLog[0] == 1){
+            knightStory += `${saveFile.name} the ${saveFile.class} saw an OLD WOMAN in the village square begging with a HAT FULL OF CHANGE. You DONATE your COINS to her. `;
         }
         if (saveFile.choiceLog[1] == 0){
-
+            knightStory += `${saveFile.name} the ${saveFile.class} spotted a HORSE that had LOST THEIR OWNER. You STOLE the HORSE away into the COLD NIGHT! `;
         } else if( saveFile.choiceLog[1] == 1){
-            
+            knightStory += `${saveFile.name} the ${saveFile.class} spottted a HORSE that had LOST THEIR OWNER. You RETURNED the HORSE to its OWNER, who wept TEARS OF JOY at their REUNION. `;
         }
         if (saveFile.choiceLog[2] == 0){
-
+            knightStory += `${saveFile.name} the ${saveFile.class} found themselves in front of a RAGING RIVER. You FORDED the RIVER but LOST all of your POSSESSIONS! `;
         } else if( saveFile.choiceLog[2] == 1){
-            
+            knightStory += `${saveFile.name} the ${saveFile.class} found themselves in front of a RAGING RIVER. You TURNED AWAY and STAYED DRY! `;
         }
         if (saveFile.choiceLog[3] == 0){
-
+            knightStory += `${saveFile.name} the ${saveFile.class} heard a loud GROWL coming from the DARK WOODS. You FIGHT the BEAST and WIN. `;
         } else if( saveFile.choiceLog[3] == 1){
-            
+            knightStory += `${saveFile.name} the ${saveFile.class} heard a loud GROWL coming from the DARK WOODS. You FLEE from the BEAST in TERROR. `;
         }
         if (saveFile.choiceLog[4] == 0){
-
+            knightStory += `${saveFile.name} the ${saveFile.class} arrived at the ALTAR of a SACRED TEMPLE smelling faintly of INCENSE. You PRAY at the ALTAR and receive a DIVINE BLESSING.`;
         } else if( saveFile.choiceLog[4] == 1){
-            
+            knightStory += `${saveFile.name} the ${saveFile.class} arrived at the ALTAR of a SACRED TEMPLE smelling faintly of INCENSE. You REST at the ALTAR and awaken to a FEAST FIT FOR A KNIGHT!`;
         }
-    } else if (saveFile.classChoice == "KNIGHT"){
-        var knightStory = "";
-        if (saveFile.choiceLog[0] == 0){
-
-        } else if( saveFile.choiceLog[0] == 1){
-            
-        }
-        if (saveFile.choiceLog[1] == 0){
-
-        } else if( saveFile.choiceLog[1] == 1){
-            
-        }
-        if (saveFile.choiceLog[2] == 0){
-
-        } else if( saveFile.choiceLog[2] == 1){
-            
-        }
-        if (saveFile.choiceLog[3] == 0){
-
-        } else if( saveFile.choiceLog[3] == 1){
-            
-        }
-        if (saveFile.choiceLog[4] == 0){
-
-        } else if( saveFile.choiceLog[4] == 1){
-            
-        }
+        alert(knightStory);
     }
 }
+// I should really come back to this later, I'm just so tired . .
+// I forgot to test as I went and had a bug, I had called saveFile.class "saveFile.classChoice" and it broke my function. Time for some rest -.-
